1cceeade947346ba4358de3ebf8098a55d5ea081
christina regis <Mcfreegis11!>
2016-03-09 19:30:08 -0800

    arrays and loops

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+alert("it works!");
+
+var classmates = ["David", "Lisa", "Allie", "Matt", "Jenny"];
+
+for (var i = 0; i < classmates.length; i++) {
+  console.log(classmates[i]);
+}
+
+var students = ["Sean", "Mari", "Carlos", "Eric", "Evan"];
+
+var index = 0;
+while(index < students.length) {
+  console.log(students[index])
+  index++
+}
